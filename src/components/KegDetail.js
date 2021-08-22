73f94329188984eee584229efc864337f7bdf69b
JuanHasbunZem <zemenarwow@gmail.com>
2021-08-21 21:05:57 -0700

    create KegDetails view

---
+++ src/components/KegDetail.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+function KegDetail(props){
+  const {keg} = props;
+  function handleSellButton() {
+    const newPints = keg.pints -= 1;
+    props.onClickingSell({name: keg.name, brand: keg.brand, price: keg.price, type: keg.type, pints: newPints, id: keg.id})
+  }
+
+  return (
+    <React.Fragment>
+      <h1>Keg Details</h1>
+      <h2>{keg.name} -- {keg.type}</h2>
+      <h2>{keg.brand}</h2>
+      <h3>${keg.price} per pint</h3>
+      <h3>Pints Left in Keg: {keg.pints}</h3>
+      <button disabled={keg.pints === 0 ? true : false} onClick={handleSellButton}>Sell a Pint</button>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+KegDetail.propTypes = {
+  keg: PropTypes.object,
+  onClickingSell: PropTypes.func
+};
+
+export default KegDetail;
